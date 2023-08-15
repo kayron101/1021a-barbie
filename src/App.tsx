5d8355041391be808a4751893ac772b6be497265
KAYRON NEVES MARTINS <06344987111@ifms.edu.br>
2023-08-15 09:20:09 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from './componentes/header/Header'
+import './App.css'
+
+function App() {
+  return (
+        </Header>
+  )
+}
+
+export default App
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header>meu Header</header>
+    )
+}
+
+export default Header
